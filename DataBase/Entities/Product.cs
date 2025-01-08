4e0db0cb9d84f2c08149911a7821d87f4010e847
Alexey Fedorov <fedorov.alexey.02@gmail.com>
2025-01-09 02:42:02 +0300

    НАчал добавление базы данных

---
+++ DataBase/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace DataBase.Entities;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } 
+    public double Quantity { get; set; }
+    public int Price { get; set; }
+    public int Sum { get; set; }
+    
+}

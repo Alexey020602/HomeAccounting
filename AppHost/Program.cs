be3597aa67f21ecabd065d1b420c4b7eb798a54d
Alexey Fedorov <fedorov.alexey.02@gmail.com>
2025-08-27 16:47:35 +0300

    Начал добавлять генерацию файла Docker Compose на основании проекта AppHost

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -2,55 +2,52 @@ using Aspire.Hosting.Docker.Resources.ComposeNodes;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Aspire.Hosting.Postgres;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.Services.AddHealthChecks();
-builder.AddDockerComposeEnvironment("docker");
+builder.AddDockerComposeEnvironment("docker")
+    .WithDashboard(dashboard =>
+    {
+        dashboard.PublishAsDockerComposeService((_, service) =>
+        {
+            service.Expose.Add("11111");
+            service.Ports.Add("11111:11111");
+        });
+    });
 
-var databaseConfigurationSection = builder.Configuration.GetSection("Database");
-var username = builder.AddParameter("Username", databaseConfigurationSection.GetValue<string>("username")!, secret: true);
-var password = builder.AddParameter("Password",databaseConfigurationSection.GetValue<string>("password")!, secret: true);
-// IResourceBuilder<ParameterResource> username = new  "home_accounting_user";
-// IResourceBuilder<ParameterResource> password = "654765as465gf4as";
+var username = builder.AddParameter("Username", secret: true);
+var password = builder.AddParameter("Password", secret: true);
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql();
+
+
 var db = builder
     .AddPostgres("db")
     .WithUserName(username)
     .WithPassword(password)
     .WithLifetime(ContainerLifetime.Persistent)
+    .WithHealthCheck("npgsql")
     .WithDataVolume()
     .WithPgAdmin()
     .AddDatabase("HomeAccounting");
 
-// var migrations = builder
-//     .AddProject<MigrationService>("migrations")
-//     .WithReference(db)
-//     .WaitFor(db);
-
 var api = builder
     .AddProject<Api>("api")
     .WithExternalHttpEndpoints()
-    // .WaitForCompletion(migrations)
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        // service.DependsOn.Add("db", new ServiceDependency(){ Condition = "service_healthy"}); 
+        // service.Ports.Add("80:${API_PORT}");
+    })
     .WithReference(db)
+    .WaitFor(db)
+    .WaitForCompletion(db)
     .WithHttpHealthCheck("/health");
 ;
-
-// var client = builder.AddProject<Client>("client")
-//     .WaitFor(api)
-//     ;
-
-// var client = builder.AddProject<WebClient>("client")
-//     .WaitFor(api);
-//
-// var yarp = builder
-//         .AddProject<Gateway>("gateway")
-//         // .AddYarp("apigateway")
-//         // .WithConfigFile("yarp.json")
-//         .WithReference(api)
-//         .WithReference(client)
-//         .WithExternalHttpEndpoints()
-//     ;
 
 builder.Build().Run();

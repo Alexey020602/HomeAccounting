dab696babc17ea8bb0fd8e6ed8b47800a15dcfa3
Alexey Fedorov <fedorov.alexey.02@gmail.com>
2025-09-08 14:47:46 +0300

    Доработал регистрацию на форме валидации

--- Authorization.UI/Registration/Validators/RegistrationModelValidator.cs
+++ Authorization.UI/Registration/Validators/RegistrationModelValidator.cs
@@ -5,20 +5,24 @@ namespace Authorization.UI.Registration.Validators;
 
 internal class RegistrationModelValidator: AbstractValidator<RegistrationModel>
 {
-    public RegistrationModelValidator(IAuthorizationApi authorizationApi)
+    public RegistrationModelValidator(/*IAuthorizationApi authorizationApi*/)
     {
         RuleFor(model => model.Login)
             .Cascade(CascadeMode.Stop)
             .NotEmpty()
             .WithMessage("Необходимо ввести логин")
-            .MustAsync(async (login, _) =>
-                !await authorizationApi.CheckLoginExist(login)
-            )
-            .WithMessage("Логин {PropertyValue} уже существует")
+            .Matches("^[a-zA-Z0-9-]")
+            .WithMessage("Логин пользователя может содержать только буквы, цифры и символ дефис (-)")
+            // .MustAsync(async (login, _) =>
+            //     !await authorizationApi.CheckLoginExist(login)
+            // )
+            // .WithMessage("Логин {PropertyValue} уже существует")
             ;
         RuleFor(model => model.UserName)
             .NotEmpty()
-            .WithMessage("Необходимо ввести имя пользователя");
+            .WithMessage("Необходимо ввести имя пользователя")
+            .Matches("^[a-zA-Zа-яА-Я0-9-]")
+            .WithMessage("Имя пользователя может содержать только буквы, цифры и символ дефис (-)");
 
         RuleFor(model => model.Password)
             .Cascade(CascadeMode.Continue)
